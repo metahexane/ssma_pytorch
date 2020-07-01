e8bf6328739191cac9072d5409cc6b7feb512025
mozyar <m.ozyar@outlook.com>
2020-07-01 03:11:38 +0200

    Add help commands and update readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ssma_pytorch
-PyTorch port of SSMA (https://github.com/DeepSceneSeg/SSMA/blob/master/models/SSMA.py)
+# Self-Supervised Model Adaptation for Multimodal Semantic Segmentation in PyTorch
+This repository contains a PyTorch reproduction of the paper by [Valada et al. (2018)](https://arxiv.org/abs/1808.03833). The authors provided their code [in Tensorflow](https://github.com/DeepSceneSeg/SSMA). The code provided in this repository, however, is an indepenent reproduction of their paper.
+
+The program can be started with python main.py.
+The help argument (-h/--help) may be used to extract detailed information about the parameters of the model.
+
+Default parameters may be found in util/parser.py.
--- src/util/parser.py
+++ src/util/parser.py
@@ -7,16 +7,27 @@ def parse_args():
     """
     parser = argparse.ArgumentParser(description="SSMA for semantic segmentation")
 
-    parser.add_argument("--data", nargs='?', default="data/RAND_CITYSCAPES/")
-    parser.add_argument("--enc-lr", nargs='?', default='[0.001, 0.0001, 0]')
-    parser.add_argument("--dec-lr", nargs='?', default='[0.001, 0.001, 0.00001]')
-    parser.add_argument("--iters", nargs='?', default='[25000, 10000, 5000]')
-    parser.add_argument("--batch", nargs='?', default='[8, 8, 8]')
-    parser.add_argument("--eval", nargs='?', default='0')
-    parser.add_argument("--save", nargs='?', default='1')
-    parser.add_argument("--save-checkpoint", nargs='?', default='5000')
-    parser.add_argument("--start", nargs='?', default='1')
-    parser.add_argument("--model", nargs="?", default="")
-    parser.add_argument("--resume", nargs="?", default="0")
+    parser.add_argument("--data", nargs='?', default="data/RAND_CITYSCAPES/",
+                        help="The path to the data folder")
+    parser.add_argument("--enc-lr", nargs='?', default='[0.001, 0.0001, 0]',
+                        help="The learning rate for the encoder per stage")
+    parser.add_argument("--dec-lr", nargs='?', default='[0.001, 0.001, 0.00001]',
+                        help="The learning rate for the decoder, eASPP and SSMA blocks per stage")
+    parser.add_argument("--iters", nargs='?', default='[150000, 100000, 50000]',
+                        help="The amount of iterations per stage")
+    parser.add_argument("--batch", nargs='?', default='[8, 12, 7]',
+                        help="The batch size per stage")
+    parser.add_argument("--eval", nargs='?', default='0',
+                        help="Whether to evaluate the model after each epoch")
+    parser.add_argument("--save", nargs='?', default='1',
+                        help="Whether to save the model")
+    parser.add_argument("--save-checkpoint", nargs='?', default='5000',
+                        help="Saves each x amount of iterations")
+    parser.add_argument("--start", nargs='?', default='1',
+                        help="The starting stage, the model parameters must also be specified if set so another value than 1")
+    parser.add_argument("--model", nargs="?", default="",
+                        help="The model date/signature")
+    parser.add_argument("--resume", nargs="?", default="0",
+                        help="Whether to continue training or to start training all over again from the stage given by the start parameter")
 
     return parser.parse_args()
